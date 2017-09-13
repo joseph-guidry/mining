9e0de684fe10a6e72e9e3fc374ec20a56f71d55b
joseph.guidry <joseph.guidry4@gmail.com>
2017-09-13 12:58:07 -0400

    Create classes for zergs with subclass Overlord and Drones

--- mining/zerg.py
+++ mining/zerg.py
@@ -1,13 +1,73 @@
-#!/usr/local/bin/python
+#! /usr/bin/env python3
 
+"""
+MODULE DOCUMENTATION
+"""
+
+import abc
 from random import randint, choice
+    
+class Zerg(metaclass=abc.ABCMeta):
+    """ Abstract zerg for API """
 
-class Drone:
-    def __init__(self):
-        self.health = 25
-        self.moves = randint(1,6)
+    @property
+    def health(self):
+        """ Requires the health attribute for all zergs"""
+        return self._health
+
+    @health.setter
+    def health(self, health):
+        self._health = health
+
+    @abc.abstractmethod 
+    def action(self, map_context):
+        """ Method required by all zerg types """
+        pass
+
+class Overlord(Zerg):
+    """ Overlord Zerg Class """
+
+    def __init__(self, total_ticks=1, refined_minerals=54):
+        """ Constuct a Overlord with default values if not provided """
+        self.health = 5
+        self.ticks = total_ticks
+        self.refined_minerals = refined_minerals
+        self.zerg = {}
+        self.maps = {}
+
+        for _ in range(6):
+            z = BaseDrone()
+            self.zerg[id(z)] = z          
+
+    def add_map(self, map_id, summary):
+        """ Allows Overlord to knwo about multiple maps? """
+        self.maps[map_id] = summary
+
+    def action(self, map_context=None):
+        """ Allows overlord to deploy, retrieve or None """
+        """ This is based on drone ID """
+        act = randint(0, 3)
+        if act == 0:
+            return 'RETURN {}'.format(choice(list(self.zerg.keys())))
+        else:
+            return 'DEPLOY {} {}'.format(choice(list(self.zerg.keys())),
+                    choice(list(self.maps.keys())))
+
+
+class Drone(Zerg):
+    """ This is base drone class """
+
+    @classmethod
+    def get_init_cost(cls):
+        """ Return the cost value for a drone object """
+        return cls.init_value
+
+    def steps(self):
+        """ return the total number of steps taken by drone """
+        return self.steps
 
     def action(self, context):
+        """ Inherited method for all Drone subclasses """
         new = randint(0, 3)
         if new == 0 and context.north in '* ':
             return 'NORTH'
@@ -20,71 +80,60 @@ class Drone:
         else:
             return 'CENTER'
 
-class Overlord:
-    def __init__(self, total_ticks):
-        self.maps = {}
-        self.zerg = {}
 
-        for _ in range(6):
-            z = Drone()
-            self.zerg[id(z)] = z
-
-    def add_map(self, map_id, summary):
-        self.maps[map_id] = summary
-
-    def action(self):
-        act = randint(0, 3)
-        if act == 0:
-            return 'RETURN {}'.format(choice(list(self.zerg.keys())))
-        else:
-            return 'DEPLOY {} {}'.format(choice(list(self.zerg.keys())),
-                    choice(list(self.maps.keys())))
-
-
-"""
-#! /usr/bin/env python3
-
-"""
-MODULE DOCUMENTATION
-"""
-
-import abc
-
+class BaseDrone(Drone):
+    """ Extend the drone class to default drones """
+    """ Health = 40, Capacity = 10, Move = 1 """
     
-class Zerg(metaclass=abc.ABCMeta):
-    """ Abstract zerg for API """
+    init_value = 9
+    
 
-    @abstractproperty
-    def health(self, value):
-        """ Requires the health attribute for all zergs"""
-        return 'This is not good'
-
-    @abc.abstractmethod 
-    def action(self, map_context):
-        """ Method required by all zerg types """
-
-class Overload(Zerg):
-    """ Overlord Zerg Class """
-
-    def __init__(self, total_ticks=1, refined_minerals=54)
-        """ Constuct a Overlord with default values if not provided """
-        pass
-
-    def add_map(self, map_id, summary):
-        """ Allows Overlord to knwo about multiple maps? """
-        pass
-
+    def __init__(self, health=4, capacity=2, moves=3):
+        """ Constructor for Base Drone, pass number of refined minerals"""
+        self.health = health * 10
+        self.capacity = capacity * 5
+        self.moves = int(moves / 3)
+        self.steps = 0
+    
     def action(self, map_context):
         """ Allows overlord to deploy, retrieve or None """
         """ This is based on drone ID """
         pass
 
 
-class Drone(Zerg):
-    """ This is base drone class """
+class ScoutDrone(Drone):
+    """ Extend Drone class to specialized scout Drone """
+    """ Health = 20, Capacity = 5, Move = 2 """
+    init_value = 9
+    steps = 0
+
+    def __init__(self, health=2, capacity=1, moves=6):
+        """ Constructor for Base Drone, pass number of refined minerals"""
+        self.health = health
+        self.capacity = capacity * 5
+        self.moves = int(moves / 3)
+        self.steps = 0
+
+    def action(self, map_context):
+        """ Allows overlord to deploy, retrieve or None """
+        """ This is based on drone ID """
+        pass
     
-    def __init__(self):
-        """ Create a base drone with default values """
-        super().__init__()
-        
-"""
+
+class HaulerDrone(Drone):
+    """ Extend Drone class to specialized hauler Drone """
+    """ Health = 20, Capacity = 20, Move = 1 """
+    init_value = 9
+    steps = 0
+
+    def __init__(self, health=2, capacity=4, moves=3):
+        """ Constructor for Base Drone, pass number of refined minerals"""
+        self.health = health
+        self.capacity = capacity * 5
+        self.moves = moves / 3
+        self.steps = 0
+
+    def action(self, map_context):
+        """ Allows overlord to deploy, retrieve or None """
+        """ This is based on drone ID """
+        pass
